885e7299c1bce2d915f0463ed4e7c5c1a3601e83
Mohib Wasay <wasay_siddiqui81@yahoo.com>
2016-07-04 03:33:47 +0500

    create stateChangeSuccess event

--- app/js/core/core.run.js
+++ app/js/core/core.run.js
@@ -8,8 +8,12 @@
 	  .run(routingEvents);
 
 	function routingEvents($rootScope, lodash){
-	  $rootScope.$on('$stateChangeStart', function(event, fromState){
-		  console.log(lodash.capitalize(fromState.name));
+	  $rootScope.$on('$stateChangeStart', function(event, toState){
+		  console.log(lodash.capitalize(toState.name));
+	  });
+
+	  $rootScope.$on('$stateChangeStart', function(event, toState){
+		  $rootScope.pageTitle = toState.title;
 	  });
 	}
 })();
