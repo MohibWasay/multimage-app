10f3afa9ca836b09f9d93bfc70dffcc579c87258
Mohib Wasay <wasay_siddiqui81@yahoo.com>
2016-07-04 03:31:08 +0500

    added directive for ngThumb

--- app/js/common/common.module.js
+++ app/js/common/common.module.js
@@ -3,25 +3,10 @@
     'use strict';
 
     var module = {
-        name: 'multimage.gallery',
+        name: 'multimage.common',
         dependencies: ['angularFileUpload']
     };
 
     angular
         .module(module.name, module.dependencies)
-        .config(configuration);
-
-    function configuration($stateProvider) {
-        $stateProvider
-            .state('gallery', {
-                url: '/gallery',
-                controller: 'Gallery as vm',
-                templateUrl: 'js/gallery/gallery.html'
-            })
-            .state('gallery.upload', {
-                url: '/upload',
-                controller: 'Upload as vm',
-                templateUrl: 'js/gallery/upload.html'
-            })
-    }
 })();
