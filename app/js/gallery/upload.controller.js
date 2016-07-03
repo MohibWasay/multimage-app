861c0f7ace5ebf262f192a939051d4793812b477
Mohib Wasay <wasay_siddiqui81@yahoo.com>
2016-07-04 01:55:33 +0500

    setup controller for upload image using controller as vm

--- app/js/gallery/upload.controller.js
+++ app/js/gallery/upload.controller.js
@@ -12,6 +12,8 @@
             url: 'upload.php'
         });
 
+        vm.uploader = uploader;
+
         uploader.filters.push({
             name: 'imageFilter',
             fn: function(item) {
@@ -20,6 +22,40 @@
             }
         });
 
-        vm.state = 'Hello From Uploader';
+        uploader.onWhenAddingFileFailed = function(item, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
     }
 })();
